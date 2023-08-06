c7c16f81f25547f6888dd8edda5e611c095f12dd
Shegzzy <miksegun1996@gmail.com>
2023-08-06 15:11:43 +0100

    added axios and making get request to the api using axios

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import { React, useEffect, useState } from 'react'
+import axios from 'axios';
 
 const HomePage = () => {
+
+    const [advocates, serAdvocates] = useState([]);
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    let getData = async () => {
+        let response = await axios.get('http://127.0.0.1:8000/advocates/')
+        serAdvocates(response.data)
+        console.log(response);
+    }
+
+
     return (
         <div>
             <h1>Home Page</h1>
